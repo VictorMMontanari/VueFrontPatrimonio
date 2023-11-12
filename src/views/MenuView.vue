b656391becab4cd61db7b15b9a53542708b21405
victormmontanari <victor9000@gmail.com>
2023-11-12 12:01:47 -0300

    Ajustes Login e Criação Mnuview

---
+++ src/views/MenuView.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <h1>Menua sdasdasd</h1>
+    <ul>
+      <li><router-link to="/about">About</router-link></li>
+      <!-- Outros itens de menu -->
+    </ul>
+  </main>
+</template>
